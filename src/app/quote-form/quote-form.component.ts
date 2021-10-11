0714092ced6d58d8a05d33f5edc183b42cb54e1d
mwendaB <brian.mwenda@moringaschool.com>
2021-10-11 09:37:46 +0300

    create eventemitters

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, NgForm } from '@angular/forms';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quote-form',
@@ -6,6 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote-form.component.css']
 })
 export class QuoteFormComponent implements OnInit {
+ 
+  @Output() addQuote = new EventEmitter<Quote>();
+
+  newQuote = new Quote("","","",0,0,new Date());
+
+  submitQuote(f:NgForm){
+    if(f.valid){
+      this.addQuote.emit(this.newQuote);
+      this.newQuote = new Quote("","","",0,0, new Date());
+    }else{
+      alert("Please enter valid inputs")
+    }
+  }
 
   constructor() { }
 
